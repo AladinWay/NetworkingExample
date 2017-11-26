6b1d7850a90abef2fe0df3e984c40951f53a444f
Alaeddine Messaoudi <aladin@MacBook-Pro-2.lan>
2017-11-26 17:35:08 +0100

    First commit.

---
+++ Networking/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Networking
+//
+//  Created by Alaeddine Messaoudi on 26/11/2017.
+//  Copyright © 2017 Alaeddine Me. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+ 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: ArticlesViewController())
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Networking/Controllers/ArticlesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ArticlesViewController.swift
+//  Networking
+//
+//  Created by Alaeddine Messaoudi on 26/11/2017.
+//  Copyright © 2017 Alaeddine Me. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+    }
+    
+}
