a7fbfb498566d62c5b7d4409ce801d28d843a419
Alaeddine Messaoudi <aladin@MacBook-Pro-2.lan>
2017-11-26 18:48:16 +0100

    Add API Router.

---
+++ Networking/APIClient/APIRouter.swift
@@ -0,0 +1,74 @@
+//
+//  APIRouter.swift
+//  Networking
+//
+//  Created by Alaeddine Messaoudi on 26/11/2017.
+//  Copyright © 2017 Alaeddine Me. All rights reserved.
+//
+
+import Alamofire
+
+enum APIRouter: URLRequestConvertible {
+    
+    case login(email:String, password:String)
+    case articles
+    case article(id: Int)
+    
+    // MARK: - HTTPMethod
+    private var method: HTTPMethod {
+        switch self {
+        case .login:
+            return .post
+        case .articles, .article:
+            return .get
+        }
+    }
+    
+    // MARK: - Path
+    private var path: String {
+        switch self {
+        case .login:
+            return "/login"
+        case .articles:
+            return "/articles"
+        case .article(let id):
+            return "/article/\(id)"
+        }
+    }
+    
+    // MARK: - Parameters
+    private var parameters: Parameters? {
+        switch self {
+        case .login(let email, let password):
+            return [K.APIParameterKey.email: email, K.APIParameterKey.password: password]
+        case .articles, .article:
+            return nil
+        }
+    }
+    
+    // MARK: - URLRequestConvertible
+    func asURLRequest() throws -> URLRequest {
+        let url = try K.ProductionServer.baseURL.asURL()
+        
+        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        
+        // HTTP Method
+        urlRequest.httpMethod = method.rawValue
+        
+        // Common Headers
+        urlRequest.setValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.acceptType.rawValue)
+        urlRequest.setValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.contentType.rawValue)
+        
+        // Parameters
+        if let parameters = parameters {
+            do {
+                urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            } catch {
+                throw AFError.parameterEncodingFailed(reason: .jsonEncodingFailed(error: error))
+            }
+        }
+        
+        return urlRequest
+    }
+}
+
---
+++ Networking/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Networking
+//
+//  Created by Alaeddine Messaoudi on 26/11/2017.
+//  Copyright © 2017 Alaeddine Me. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct ProductionServer {
+        static let baseURL = "http://itechnodev.com/api"
+    }
+    
+    struct APIParameterKey {
+        static let password = "password"
+        static let email = "email"
+    }
+}
+
+enum HTTPHeaderField: String {
+    case authentication = "Authorization"
+    case contentType = "Content-Type"
+    case acceptType = "Accept"
+    case acceptEncoding = "Accept-Encoding"
+}
+
+enum ContentType: String {
+    case json = "application/json"
+}
