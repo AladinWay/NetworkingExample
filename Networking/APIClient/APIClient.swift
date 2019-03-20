410c3daed3432a3c64a813e6b0a8e5aedc31678c
Alaeddine Messaoudi <>
2019-03-20 21:30:18 +0100

    Update to Alamofire 5.0.0 beta 3

--- Networking/APIClient/APIClient.swift
+++ Networking/APIClient/APIClient.swift
@@ -11,8 +11,8 @@ import Alamofire
 class APIClient {
     @discardableResult
     private static func performRequest<T:Decodable>(route:APIRouter, decoder: JSONDecoder = JSONDecoder(), completion:@escaping (Result<T>)->Void) -> DataRequest {
-        return Alamofire.request(route)
-                        .responseJSONDecodable (decoder: decoder){ (response: DataResponse<T>) in
+        return AF.request(route)
+                        .responseDecodable (decoder: decoder){ (response: DataResponse<T>) in
                             completion(response.result)
         }
     }
