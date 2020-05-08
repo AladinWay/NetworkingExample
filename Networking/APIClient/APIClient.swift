a66652f83bd49a1b0a6ed6db4c60cd03ff99f194
Alaeddine Messaoudi <>
2020-05-08 22:12:39 +0200

    Update to Swift 5

--- Networking/APIClient/APIClient.swift
+++ Networking/APIClient/APIClient.swift
@@ -10,18 +10,18 @@ import Alamofire
 
 class APIClient {
     @discardableResult
-    private static func performRequest<T:Decodable>(route:APIRouter, decoder: JSONDecoder = JSONDecoder(), completion:@escaping (Result<T>)->Void) -> DataRequest {
+    private static func performRequest<T:Decodable>(route:APIRouter, decoder: JSONDecoder = JSONDecoder(), completion:@escaping (Result<T, AFError>)->Void) -> DataRequest {
         return AF.request(route)
-                        .responseDecodable (decoder: decoder){ (response: DataResponse<T>) in
+                        .responseDecodable (decoder: decoder){ (response: DataResponse<T, AFError>) in
                             completion(response.result)
         }
     }
     
-    static func login(email: String, password: String, completion:@escaping (Result<User>)->Void) {
+    static func login(email: String, password: String, completion:@escaping (Result<User, AFError>)->Void) {
         performRequest(route: APIRouter.login(email: email, password: password), completion: completion)
     }
     
-    static func getArticles(completion:@escaping (Result<[Article]>)->Void) {
+    static func getArticles(completion:@escaping (Result<[Article], AFError>)->Void) {
         let jsonDecoder = JSONDecoder()
         jsonDecoder.dateDecodingStrategy = .formatted(.articleDateFormatter)
         performRequest(route: APIRouter.articles, decoder: jsonDecoder, completion: completion)
